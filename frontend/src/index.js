525463711d209c4716a868dfc35b960df22a6b62
Patrick Ondreovici <ondreovicip@gmail.com>
2026-06-07 12:30:54 +0300

    leaflet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
